48da85f1faf900f68a3806427bf5008571624331
unknown <agraton12@gmail.com>
2021-04-27 00:42:04 -0500

    --

--- app/__init__.py
+++ app/__init__.py
@@ -17,7 +17,7 @@ login_manager.init_app(app)
 login_manager.login_view = 'login'
 
 csrf = CSRFProtect(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'qanljzjqnupgxj:b43f7bf6db741b33083e5e0eb6dd37a5e272e41064685dc43552e755cf0c9695@ec2-3-234-85-177.compute-1.amazonaws.com:5432/d36qf35btbhibq'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://qanljzjqnupgxj:b43f7bf6db741b33083e5e0eb6dd37a5e272e41064685dc43552e755cf0c9695@ec2-3-234-85-177.compute-1.amazonaws.com:5432/d36qf35btbhibq'
 app.config.from_object(Config)
 
 from app import views
